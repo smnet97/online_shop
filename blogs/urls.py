e49bb1f1593568e87b2e4a323d6d7de2742928cd
smnet97 <sardor.mahkamov0701@gmail.com>
2022-07-15 11:04:20 +0500

    added requirements.txt

--- blogs/urls.py
+++ blogs/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import PostListView
+from .views import PostListView, PostDetailView
 
 app_name = 'blogs'
 
 urlpatterns = [
-    path('', PostListView.as_view(), name='posts')
+    path('', PostListView.as_view(), name='posts'),
+    path('<int:pk>/post/', PostDetailView.as_view(), name='detail')
 ]
--- blogs/views.py
+++ blogs/views.py
@@ -4,8 +4,14 @@ from .models import PostModel
 
 
 class PostListView(ListView):
-    queryset = PostModel.objects.order_by('-id')
     template_name = 'blog.html'
+
+    def get_queryset(self):
+        qs = PostModel.objects.order_by('-id')
+        tag = self.request.GET.get('tag')
+        if tag:
+            qs = qs.filter(tag__name=tag)
+        return qs
 
 
 class PostDetailView(DetailView):
